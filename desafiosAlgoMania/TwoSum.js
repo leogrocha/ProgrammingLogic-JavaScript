8b074212af3cd14988695e4eb11f6192abe6c49b
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-05-16 08:21:04 -0300

    two sum

--- desafiosAlgoMania/TwoSum.js
+++ desafiosAlgoMania/TwoSum.js
@@ -6,7 +6,7 @@
 
 // -2 + 11 = 9
 
-const numbers = [4,1,2, -2,11,15,1,-1,-6,-4];
+const numbers = [4,1,2,-2,11,15,1,-1,-6,-4];
 const target_sum = 9;
 
 // Resposta utilizando on2
@@ -28,19 +28,19 @@ const target_sum = 9;
 function solution(numbers, target_sum){
     let result = [];
 
-    for(let i = 0; i < numbers.length; i++){
-        let first = numbers[i];
-        let second = target_sum - first;
-        if(numbers.includes(second)){
-            return result = [first, second];
-        } else {
-            return result = [];
+    for(let i = 0; i < numbers.length-1; i++){
+        //console.log(`${numbers[i]} - ${numbers[i+1]}`);
+        if(numbers[i] + numbers[i+1] === target_sum){
+            result.push(numbers[i], numbers[i+1]);
         }
     }
+
+    return result;
 
 }
 
 console.log(solution(numbers, target_sum));
+//solution(numbers, target_sum);
     
 
 
