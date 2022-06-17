736e537e3790bf54a1ce83469d50f8acfc443177
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-06-17 20:20:29 -0300

    mmc dois números finalizado

--- problemsMath/mmc_dois_numeros.js
+++ problemsMath/mmc_dois_numeros.js
@@ -1,7 +1,21 @@
 // mmc - dois numeros
 
 function mmc(number1, number2) {
-    
+    let status = false;
+    let contador = 1;
+    let result = 0;
+
+    while(status === false){
+        let contador_number2 = number2 * contador;
+        if(contador_number2 % number1 === 0){
+            result = number2 * contador;
+            status = true;
+        }
+
+        contador++;
+    }
+
+    return result;
 }
 
 console.log(mmc(8,6));
