05b6cf868daa4082344e5d02c1b58707c78fada6
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-06-17 21:45:35 -0300

    fracao_mista

---
+++ problemsMath/fracao_mista.js
@@ -0,0 +1,6 @@
+function fracao_mista(inteiro, numerador, denominador){
+    return ((inteiro * numerador) + denominador) + " / " + numerador;
+}
+
+console.log(fracao_mista(9, 10, 7));
+console.log(fracao_mista(6, 12, 1));
