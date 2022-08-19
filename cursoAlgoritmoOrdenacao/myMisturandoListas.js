08bd0b0bcf2cabb3995819c7b999b147037cabc8
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-08-19 16:55:38 -0300

    Create myMisturandoListas.js

---
+++ cursoAlgoritmoOrdenacao/myMisturandoListas.js
@@ -0,0 +1,22 @@
+const editoraFolha = [
+    {nome: 'PHP', preco: 15},
+    {nome: 'JavaScript', preco: 25},
+    {nome: 'Java', preco: 30},
+    {nome: 'Go', preco: 45},
+    {nome: 'Elixir', preco: 50},
+]
+
+
+const editoraGalho = [
+    {nome: 'Python', preco: 20},
+    {nome: 'Rust', preco: 22},
+    {nome: 'Ruby', preco: 28},
+    {nome: 'C#', preco: 33},
+    {nome: 'C++', preco: 35},
+    {nome: 'Scala', preco: 40},
+]
+
+/*
+    Criar um algoritmo para poder misturar as duas listas
+    1. Verificar o elemento de ambas as listas e o menor será enviado primeiro
+*/
