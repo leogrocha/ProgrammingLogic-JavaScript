4f6c8ff27923f992438ad8910276a6dd5e00a1af
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-09-24 11:25:15 -0300

    Finish Game Genius

--- HtmlCssJavaScript/genius/script.js
+++ HtmlCssJavaScript/genius/script.js
@@ -3,7 +3,8 @@ const min = 0;
 const max = 3;
 var select = [];
 var sort = [];
-var cont = 0;
+var counter = 0;
+var colors = [];
 
 
 // Função para gerar os números aleatórios que serão as cores
@@ -17,67 +18,92 @@ function startGame() {
 
     select = [];
     sort = [];
-    cont = 0;
+    counter = 0;
+    colors = [];
+    document.getElementById('counter').innerHTML = counter;
 
-    setTimeout(createColors, 1000);
-
+    setTimeout(createColors, 300);
 }
 
 function createColors() {
 
+    // Altera o banner de status
     document.querySelector('.wait').style.display = 'flex';
     document.querySelector('.go').style.display = 'none';
 
-    let color = "";
-
+    // Gerar número aleatório e adicionar no array
     var numberSort = numberRandom(min, max);
     sort.push(numberSort);
+    if(numberSort == 0) {
+        colors.push('blue'); 
+    } else if (numberSort == 1) {
+        colors.push('red');
+    } else if (numberSort == 2) {
+        colors.push('green');
+    } else if (numberSort == 3) {
+        colors.push('yellow');
+    }
 
+    // Variáveis de tempo de início e fim da exibição das cores
     var openColor = 1000;
     var closedColor = 0;
 
+    // For para percorrer o array sort e ir implementando as cores geradas
     for(let i = 0; i < sort.length; i++) {
-        
-        if(sort[i] == 0) {
-            color = "blue";
-        } else if(sort[i] == 1) {
-            color = "red";
-        } else if(sort[i] == 2) {
-            color = "green";
-        } else if(sort[i] == 3) {
-            color = "yellow";
-        }
-
-        openColor += 1000;
+        openColor += 1500;
         closedColor = openColor + 1000;
 
         setTimeout(() => {
-            document.getElementById(sort[i]).style.backgroundColor = color;
+            document.getElementById(sort[i]).style.backgroundColor = colors[i];
         }, openColor)
 
         setTimeout(() => {
             document.getElementById(sort[i]).style.backgroundColor = '';
         }, closedColor)
     }
-
-
+    
+    setTimeout(selectColors, closedColor + 1000);
+    
 }
 
 function selectItem(id) {
 
-    console.log('ID SELECT: ' + id);
     select.push(id);
-    console.log('ARRAY SELECT: ' + '[' + select + ']');
-
     
-
-
-
+    selectColors();
 }
 
+function selectColors() {
+    document.querySelector('.wait').style.display = 'none';
+    document.querySelector('.go').style.display = 'flex';
+    let equals = true;
 
+    for(let i = 0; i < select.length; i++) {
+        if(sort[i] != select[i]) {
+            equals = false;
+            ToastSnack("Erooooou! Tente Novamente", 3000, '10px', 'erro');
+            setTimeout(startGameLost, 4000);
+        }
 
+        if(select.length == sort.length) {
+            select = [];
+            counter++;
+            document.getElementById('counter').innerHTML = counter;
+            createColors();
+        }
+    }
+}
 
+function startGameLost() {
+    document.querySelector('.wait').style.display = 'flex';
+    document.querySelector('.go').style.display = 'none';
+
+    select = [];
+    sort = [];
+    counter = 0;
+    colors = [];
+    document.getElementById('counter').innerHTML = counter;
+}
 
 function ToastSnack(msg, tempo, margin, tipo) {
     var tempo = tempo == undefined ? 4000 : tempo;
