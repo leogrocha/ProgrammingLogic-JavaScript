99d508ec7187cb1ab9213fb12497072114e40095
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-10-02 12:01:05 -0300

    Finish Curso Consumo de Dados Alura

--- API/ConsumoApiAlura/script.js
+++ API/ConsumoApiAlura/script.js
@@ -17,23 +17,36 @@
 // console.log(consultaCEP);
 
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = '';
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`)
         var consultaCEPConvertida = await consultaCEP.json();
         if(consultaCEPConvertida.erro) {
             throw Error('CEP não existe!')
         }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endeereco');
+        var estado = document.getElementById('estado');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEP.logradouro;
+        estado.value = consultaCEP.uf;
 
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
         console.log(erro);
+        mensagemErro.innerHTML = '<p> CEP inválido. Tente novamente</p>';
     }
 }
-
+ 
 
 //PromisseAll
-let ceps = ['01001000', '01001001', '37950000']
-let conjuntosCep = ceps.map(valores => buscaEndereco(valores));
-Promise.all(conjuntosCep).then(respostas => console.log(respostas));
+// let ceps = ['01001000', '01001001', '37950000']
+// let conjuntosCep = ceps.map(valores => buscaEndereco(valores));
+// Promise.all(conjuntosCep).then(respostas => console.log(respostas));
+
+var cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
 
