838b2b8a64ee5167fc2cdbf77ef9e53918a53c7d
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-12-01 12:24:47 -0300

    Teste Objetos

---
+++ objects/myObjects/Banco.js
@@ -0,0 +1,23 @@
+// class Banco {
+//     constructor(nome, saldo) {
+//         this.nome = nome;
+//         this.saldo = saldo;
+//     }
+
+//     deposito(valor) {
+//         this.saldo += valor;
+//     }
+// }
+
+// const banco = new Banco('Seu Madruga', 500.00);
+// banco.deposito(10.00);
+// console.log(banco);
+
+class Teste {
+    message(){
+        return 'Hello world';
+    }
+}
+
+const teste = new Teste();
+console.log(teste.message());
