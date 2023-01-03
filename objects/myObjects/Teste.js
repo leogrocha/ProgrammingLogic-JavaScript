414fc6e1cec76552267ed7b569435afd063fa25a
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-01-03 17:03:47 -0300

    Update Teste.js

--- objects/myObjects/Teste.js
+++ objects/myObjects/Teste.js
@@ -1,19 +1,31 @@
-class Funcionario {
+class Pessoa {
     id;
     nome;
-    salario;
-    departamento;
+    documento;
+    data_nascimento;
+    email;
 
-    aumentoSalario(valor) {
-        this.salario += valor;
+    constructor(id, nome, documento, data_nascimento,email){
+        this.id = id;
+        this.nome = nome;
+        this.documento = documento;
+        this.data_nascimento = data_nascimento;
+        this.email = email;
     }
 }
 
-const funcionario = new Funcionario();
-funcionario.id = 1;
-funcionario.nome = 'Seu Madruga';
-funcionario.salario = 5000.00;
-funcionario.departamento = 'Development';
-console.log(funcionario);
-funcionario.aumentoSalario(1000);
-console.log(funcionario);
+const pessoa = new Pessoa(1,'Seu Madruga', '789.456.123-99', '1980-01-01', 'contato@hotmail.com');
+const pessoa2 = new Pessoa(2,'Chaves', '154.789.455-88', '1990-01-01', 'contato@hotmail.com');
+const pessoa3 = new Pessoa(3,'Quico', '123.789.455-88', '1970-01-01', 'contato@hotmail.com');
+const pessoa4 = new Pessoa(4,'Seu Barriga', '456.789.455-88', '1985-01-01', 'contato@hotmail.com');
+const arrayObjetos = [];
+arrayObjetos.push(pessoa);
+arrayObjetos.push(pessoa2);
+arrayObjetos.push(pessoa3);
+arrayObjetos.push(pessoa4);
+
+arrayObjetos.forEach(pessoa => console.log(pessoa.id));
+arrayObjetos.forEach(pessoa => console.log(pessoa.nome));
+
+
+
