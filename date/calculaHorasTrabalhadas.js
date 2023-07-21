be81deabd3217b58acb1bd9604ee20b63f892c45
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-07-21 16:32:56 -0300

    Calcula horas trabalhadas

---
+++ date/calculaHorasTrabalhadas.js
@@ -0,0 +1,22 @@
+function retornaMillisegundos(horarioInicial, horarioFinal) {
+    var startTime = new Date(`1970-01-01T${horarioInicial}`);
+    var endTime = new Date(`1970-01-01T${horarioFinal}`);
+    return endTime - startTime;
+}
+
+function calculaDiferencaTempo(millisegundos) {
+    const totalSegundos = Math.floor(millisegundos / 1000);
+    const horas = Math.floor(totalSegundos / 3600);
+    const minutos = Math.floor((totalSegundos % 3600) / 60);
+    const segundos = totalSegundos % 60;
+    
+    return {
+        totalSegundos: totalSegundos,
+        horas: horas,
+        minutos: minutos,
+        segundos: segundos
+    };
+}
+
+console.log(calculaDiferencaTempo(retornaMillisegundos('08:00:00', '17:18:00') - retornaMillisegundos('13:00:00', '13:30:00')));
+
