e932831c7239aaea524273354029f043753f4186
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-07-25 00:08:42 -0300

    Melhorando estrutura de um código random

--- others/random.js
+++ others/random.js
@@ -2,17 +2,14 @@ const qtdeElementos = 30;
 const min = 1;
 const max = 30;
 
-function gerarNumerosAleatorios(min, max) {
-    return (Math.random() * (max - min) + min).toFixed(0);
-}
+const gerarNumerosAleatorios = (min, max) => (Math.random() * (max - min) + min).toFixed(0);
+    
 
 function arrayComNumerosCriados() {
     let elements = new Set();
 
     while(elements.size != qtdeElementos) {
-        for(let i = 0; i < qtdeElementos; i++){
-            elements.add(Number(gerarNumerosAleatorios(min, max)));
-        }   
+        elements.add(Number(gerarNumerosAleatorios(min, max)));
     }
 
     return [...elements];
