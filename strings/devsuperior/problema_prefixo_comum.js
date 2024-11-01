3ee5887ddcf656ada3b7612a1985068cde2f72ab
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2024-11-01 01:18:19 -0300

    refactor: finished prefixo_comum

--- strings/devsuperior/problema_prefixo_comum.js
+++ strings/devsuperior/problema_prefixo_comum.js
@@ -1,32 +1,60 @@
-const longestCommonPrefix = (v) => {
+const longestCommonPrefix = (strs) => {
 
-    let lowestIndex = v[0].length;
+    let lowestIndex = strs[0].length;
     let listOfPrefixs = [];
+    let equal = true;
+    let prefix = "";
+    let temp = "";
 
-    for (let i = 0; i < v.length; i++) {
-        if (v[i].length < lowestIndex) {
-            lowestIndex = v[i].length
+    for (let i = 0; i < strs.length; i++) {
+        if (strs[i].length < lowestIndex) {
+            lowestIndex = strs[i].length
         }
     }
 
     for (let j = 0; j < lowestIndex; j++) {
-        // let letter = "";
-        // let temp = "";
-        for (let k = 0; k < v.length; k++) {
-
-            // letter = v[k][j]
-            // if (letter != temp) {
-                
-            // }
-
-            // if (temp == v[k][j]) {
-            //     prefix += v[k][j]
-            // }
-            console.log(`INDICE DO ARRAY ${k} E LETRA ${j}: ${v[k][j]}`);
+        for (let k = 0; k < strs.length; k++) {
+            temp += strs[k][j]
         }
+
+        for (let w = 1; w < temp.split("").length; w++) {
+            if (temp[0] !== temp[w]) {
+                equal = false;
+                break;
+            }
+        }
+
+        if (equal) {
+            prefix += temp[0]
+        } else {
+            listOfPrefixs.push(prefix);
+            return prefix;
+        }
+
+
+        temp = "";
+        equal = true;
     }
 
-    console.log(`MENOR INDICE: ${lowestIndex}`);
+    return prefix;
+
+    // listOfPrefixs.push(prefix);
+
+
+    // let indice = 0;
+    // let bigger = 0;
+
+    // for (let z = 0; z < listOfPrefixs.length; z++) {
+    //     if (listOfPrefixs[z].length > bigger) {
+    //         bigger = listOfPrefixs[z].length;
+    //         indice = z;
+    //     }
+    // }
+
+    // console.log(listOfPrefixs);
+
+    // return listOfPrefixs[indice];
 }
 
-longestCommonPrefix(['flowers', 'flow', 'flight']);
+console.log(longestCommonPrefix(['flowers', 'flow', 'flight']));
+console.log(longestCommonPrefix(["flower","fkow"]));
