dc2a831a1cef282065b21f332d8497d64c7b5c03
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2024-11-08 00:23:01 -0300

    feat: finished problem system integrations

--- strings/devsuperior/problema_transaction.js
+++ strings/devsuperior/problema_transaction.js
@@ -1,27 +1,49 @@
-const invalidTransactions = (transactions) => {
+function invalidtransactions(transactions) {
+    const result = [];
+    const invalidArray = new Array(transactions.length).fill(false);
 
-    let listOfTransactions = []
+    for (let i = 0; i < transactions.length; i++) {
+        const ti = new Transaction(transactions[i]);
 
-    for (let item of transactions) {
-        let transactionSplit = item.split(",")
+        if (ti.amount > 1000) {
+            invalidArray[i] = true;
+        }
 
-        listOfTransactions.push({
-            "name": transactionSplit[0],
-            "time": Number(transactionSplit[1]),
-            "amount": Number(transactionSplit[2]),
-            "city": transactionSplit[3]
-        })
+        for (let j = i + 1; j < transactions.length; j++) {
+            const tj = new Transaction(transactions[j]);
+    
+            if (ti.name === tj.name && Math.abs(ti.time - tj.time) <= 60 && ti.city !== tj.city) {
+                invalidArray[i] = true;
+                invalidArray[j] = true;
+            }
+        }
     }
 
-    console.log(listOfTransactions)
-
-    for (let index in listOfTransactions) {
-        let transaction = listOfTransactions[index];
-
-        let valueExceeded = transaction.amount > 1000;
-
-        console.log("Value Exceeded: " + valueExceeded);
+    for (let i = 0; i < transactions.length; i++) {
+        if(invalidArray[i]) {
+            result.push(transactions[i]);
+        }
     }
+
+    return result;
 }
 
-invalidTransactions(["alice,20,800,mtv", "alice, 50, 100, beijing"]);
+
+function Transaction(csv) {
+    const parts = csv.split(",");
+    this.name = parts[0];
+    this.time = Number(parts[1]);
+    this.amount = Number(parts[2]);
+    this.city = parts[3];
+}
+
+// invalidTransactions(["alice,20,800,mtv", "alice, 50, 100, beijing"]);
+
+const transactions = ["alice,20,800,mtv", "alice,50,1200,beijing", "bob,60,300,sp", "ana,70,1100, sp"];
+
+// const t1 = new Transaction(transactions[0]);
+// const t2 = new Transaction(transactions[1]);
+
+// console.log(t1, t2);
+
+console.log(invalidtransactions(transactions));
--- test.js
+++ test.js
@@ -1,25 +1,6 @@
-let map = new Map();
+const myObj = {name: "John", age: 31, city: "New York"};
+const list = [myObj];
 
-const transaction1 = {
-    id: 1,
-    valor: 10
-}
+const myJSON = JSON.stringify(list);
 
-const transaction2 = {
-    id: 2,
-    valor: 20
-}
-
-const transaction3 = {
-    id: 3,
-    valor: 30
-}
-
-map.set("chaves", [transaction1])
-
-map.set("chaves", [...map.get("chaves"), transaction2])
-
-map.set("quico", map.has("quico") ? [...map.get("quico"), transaction3] : transaction3)
-
-
-console.log(map);
+console.log(myJSON);
