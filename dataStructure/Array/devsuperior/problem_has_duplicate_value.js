9d4f3a20a5f17c5d2f72c744c58f337fefcf8064
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2024-11-09 10:47:18 -0300

    perf: ajustment perf exercise hasDuplicateValue

--- dataStructure/Array/devsuperior/problem_has_duplicate_value.js
+++ dataStructure/Array/devsuperior/problem_has_duplicate_value.js
@@ -1,12 +1,6 @@
 const hasDuplicateValue = (nums) => {
-
-    let startLength = nums.length;
-    let listSet = new Set();
-    for (let index = 0; index < startLength; index++) {
-        listSet.add(nums[index]);
-    }
-
-    return startLength !== listSet.size;
+    let listSet = new Set(nums);
+    return nums.length !== listSet.size;
 }
 
 const nums = [1,2,3,1]
