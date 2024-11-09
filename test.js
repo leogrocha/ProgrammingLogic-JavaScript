2d2e7763a76a1651919bf3d08835fec3667411ac
Leonardo Rocha <leogrocha1998@gmail.com>
2024-11-09 00:13:43 -0300

    chore: fix

--- test.js
+++ test.js
@@ -3,7 +3,8 @@ let listOfPersons = [];
 for (let index = 1; index <= 10; index++) {
     listOfPersons.push({
         id: index,
-        name: "Person"+index
+        name: "Person"+index,
+        document: "Document"+index
     });
 }
 
