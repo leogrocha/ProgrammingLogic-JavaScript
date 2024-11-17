c273350f3db7eeda4841dfe0cd4f33b7003aa7d9
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2024-11-17 10:56:14 -0300

    refactor: finished examples fibonacci

--- recursion/devsuperior/fibonacci.js
+++ recursion/devsuperior/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = (n) => {
-  return n;
+const fibonacciIneficiente = (n) => {
+  if (n == 0) {
+    return 0;
+  }
+
+  if (n == 1) {
+    return 1;
+  }
+
+  return fibonacciIneficiente(n -1) + fibonacciIneficiente(n-2);
 }
 
-console.log(fibonacci(6));
+const fibonacci = (n, a, b) => {
+  if (n == 0) {
+    return a;
+  }
+
+  if (n == 1) {
+    return b;
+  }
+
+  return fibonacci(n-1, b, a+b);
+
+} 
+
+
+
+console.log(fibonacciIneficiente(6));
+console.log(fibonacci(6,0,1));
 
