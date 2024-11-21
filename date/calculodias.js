c9ad68d922e0d4acfe5b093ddd2e772caad8eeee
leorochaectare <147871649+leorochaectare@users.noreply.github.com>
2024-11-21 17:19:07 -0300

    refactor: adjusting function date

--- date/calculodias.js
+++ date/calculodias.js
@@ -1,19 +1,17 @@
-// função para poder pegar a data de hoje e diminuir 7 dias
-function diminuir_7Dias(data) {
+function diminuir_Dias(data, day) {
     var data = new Date(data);
-    data.setDate(data.getDate() - 7);
+    data.setDate(data.getDate() - day);
     return data;
 }
 
-console.log(diminuir_7Dias("2020-05-23"));
+console.log(diminuir_Dias("2020-05-23", 7));
 
-console.log(diminuir_7Dias("2020-05-23"));
+console.log(diminuir_Dias("2020-05-23", 7));
 
-// função para podermos pegar a data de hoje e diminuir 7 dias formatando a data
-function diminuir_7Dias_formatado(data) {
+function diminuir_Dias_formatado(data, day) {
     var data = new Date(data);
-    data.setDate(data.getDate() - 7);
+    data.setDate(data.getDate() - day);
     return data.toLocaleDateString();
 }
 
-console.log(diminuir_7Dias_formatado("2022-05-23"));
+console.log(diminuir_Dias_formatado("2022-05-23", 7));
