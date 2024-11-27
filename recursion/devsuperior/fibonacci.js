06a030a1449ac59b0cc4c0ba9ca8fc2df30ab831
leorochaectare <147871649+leorochaectare@users.noreply.github.com>
2024-11-27 15:16:03 -0300

    feat: added new example fibonacci with for

--- recursion/devsuperior/fibonacci.js
+++ recursion/devsuperior/fibonacci.js
@@ -20,11 +20,27 @@ const fibonacci = (n, a, b) => {
   }
 
   return fibonacci(n-1, b, a+b);
-
 } 
 
+const fibonacciFor = (n) => {
+  if (n <= 1) {
+    return n;
+  }
 
+  let a = 0;
+  let b = 1;
+  let temp = b;
 
-console.log(fibonacciIneficiente(6));
-console.log(fibonacci(6,0,1));
+  for (let index = 1; index < n; index++) {
+    temp = b;
+    b = a + b;
+    a = temp;
+  }
+
+  return b;
+}
+
+// console.log(fibonacciIneficiente(5));
+// console.log(fibonacci(100,0,1));
+console.log(fibonacciFor(8));
 
