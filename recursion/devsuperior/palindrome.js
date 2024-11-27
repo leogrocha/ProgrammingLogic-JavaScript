136367152411cf962f5e2eae77a0e32844c9ec96
leorochaectare <147871649+leorochaectare@users.noreply.github.com>
2024-11-27 14:56:53 -0300

    feat: added new example isPalindrome two pointers

--- recursion/devsuperior/palindrome.js
+++ recursion/devsuperior/palindrome.js
@@ -16,6 +16,30 @@ const isPalindromeTailRecursive = (text, index) => {
     return isNotEquals ? false : isPalindromeTailRecursive(text, index + 1);
 }
 
+const isPalindromeTwoPointers = (text) => {
+    if (text.length===0){
+        return true;
+    }
+
+    let p1 = 0;
+    let p2 = text.length - 1;
+
+    for (let index = 0; index < text.length/2; index++) {
+        if (text[p1] != text[p2]) {
+            return false;
+        }
+
+        p1++;
+        p2--;
+    }
+
+    return true;
+}
+
 console.log(isPalindrome('abccba'));
-console.log(isPalindrome('aba'));
-console.log(isPalindrome('abaa'));
+// console.log(isPalindrome('aba'));
+// console.log(isPalindrome('abaa'));
+
+console.log(isPalindromeTwoPointers('abccba'));
+// console.log(isPalindromeTwoPointers('aba'));
+// console.log(isPalindromeTwoPointers('abaa'));
