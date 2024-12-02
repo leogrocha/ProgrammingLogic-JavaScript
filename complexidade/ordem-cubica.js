033b2ded67c089b118481017d5daaae3f1cbbf0c
leorochaectare <147871649+leorochaectare@users.noreply.github.com>
2024-12-02 17:55:03 -0300

    feat: created new example matrix multiply

---
+++ complexidade/ordem-cubica.js
@@ -0,0 +1,18 @@
+function matrixMultiply(A,B) {
+
+    const m = A.length;
+    const p = A[0].length;
+    const n = B[0].length;
+
+    const C = new Array(m).fill().map(() => new Array(n)).fill(0);
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            for (let k = 0; k < p; k++) {
+                C[i,j] += A[i, k] * B[k, j];
+            }
+        }
+    }
+}
+
+console.log(matrixMultiply([[1,2,3], [4,5,6]], [[7,8], [9,10], [11,12]]));
