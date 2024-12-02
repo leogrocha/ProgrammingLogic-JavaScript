9406f17bcc94173dc5d6208db18c955d29ae3a84
leorochaectare <147871649+leorochaectare@users.noreply.github.com>
2024-12-02 18:01:58 -0300

    refactor: finished example matrix multiply

--- complexidade/ordem-cubica.js
+++ complexidade/ordem-cubica.js
@@ -4,15 +4,17 @@ function matrixMultiply(A,B) {
     const p = A[0].length;
     const n = B[0].length;
 
-    const C = new Array(m).fill().map(() => new Array(n)).fill(0);
+    const C = new Array(m).fill().map(() => new Array(n).fill(0));
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             for (let k = 0; k < p; k++) {
-                C[i,j] += A[i, k] * B[k, j];
+                C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
+
+    return C;
 }
 
 console.log(matrixMultiply([[1,2,3], [4,5,6]], [[7,8], [9,10], [11,12]]));
